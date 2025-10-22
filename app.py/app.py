e1804e9bb954a095631f591a6b7c78c17ecd4083
anhthu2212 <anhthunguyen221206@gmail.com>
2025-10-22 18:36:19 +0700

    Update app.py

--- app.py/app.py
+++ app.py/app.py
@@ -9,3 +9,16 @@ if __name__ == "__main__":
  print("Chào mừng đến với ứng dụng To-Do List!")
  add_task("Học bài Git và GitHub")
  add_task("Làm bài tập thực hành ở nhà")
+
+def delete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        removed = tasks.pop(task_index)
+        print(f" Đã xóa: {removed['name']}")
+    else:
+        print(" Chỉ số không hợp lệ.")
+if __name__ == "__main__":
+    add_task("Học Git")
+    add_task("Làm bài tập ToDo")
+    list_tasks()
+    delete_task(0)
+    list_tasks()
